a89ebdbb283d857451475f09ada4d3e7ca5e9a0e
Matthew Keatley <matthew.keatley@nordstrom.com>
2019-09-01 09:19:42 -0700

    add comments on verifyRequest functions

--- api/lib/verifyRequest.js
+++ api/lib/verifyRequest.js
@@ -1,8 +1,19 @@
 class VerifyRequest {
-	missing(body, properties) { return properties.filter(r => !body[r]) };
+	/**
+	 * check request body to make sure it contains all required properties
+	 * @param {Object} body the req.body
+	 * @param {Array}  required property names that should be in request
+	 */
+	missing(body, required) { return required.filter(r => !body[r]) };
 
-	strip(body, properties) {
-		return properties.filter(r => body[r]).reduce((acc, curr) => {
+	/**
+	 * strip out properties from req.body that are not in the table definition 
+	 * or are not updarteable
+	 * @param {*} body the req.body
+	 * @param {*} allowed property names allowed in the request
+	 */
+	strip(body, allowed) {
+		return allowed.filter(r => body[r]).reduce((acc, curr) => {
 			acc[curr] = body[curr];
 			return acc;
 		},{});
