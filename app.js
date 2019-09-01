55bc54be1b8ab933842bb8a06d75784441c23bdc
Matthew Keatley <matthew.keatley@nordstrom.com>
2019-09-01 09:50:46 -0700

    app.js

--- app.js
+++ app.js
@@ -2,26 +2,14 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './routes';
-import mysql from 'mysql';
-
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/config/config.json')[env];
+import router from './routes/routes';
 
 const app = express();
 
-// Parse incoming requests data
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
-
-const connection = mysql.createConnection(config.sql);
-
-connection.connect((err) => {
-  if (err) throw err
-  console.log('You are now connected with mysql database...')
-})
 
 const PORT = process.env.PORT || 5000;
 
