ac71254fc33fd211e3187d523a91370014f5c5ef
Matthew Keatley <matthew.keatley@nordstrom.com>
2019-08-31 22:08:30 -0700

    move run.sql to separate sql folder

---
+++ sql/run.sql
@@ -0,0 +1,33 @@
+-- DROP DATABASE schedulingDB;
+DROP USER IF EXISTS 'scheduling'@'localhost';
+CREATE USER 'scheduling'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pw';;
+
+DROP DATABASE IF EXISTS schedulingDB;
+CREATE DATABASE schedulingDB;
+
+GRANT ALL PRIVILEGES ON schedulingDB.* TO 'scheduling'@'localhost';
+
+Use schedulingDB;
+CREATE TABLE users (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   firstName VARCHAR(40) NOT NULL,
+   lastName VARCHAR(40) NOT NULL,
+   email VARCHAR(255),
+   phone VARCHAR(20),
+   isManager BOOLEAN,  
+   contactMethod VARCHAR(20)
+);
+
+Use schedulingDB;
+CREATE TABLE shifts (
+   	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	fk_user INT NOT NULL,
+	startTime DATETIME NOT NULL,
+	endTime DATETIME NOT NULL,
+	available BOOLEAN,
+	FOREIGN KEY(fk_user) REFERENCES users(id)
+);
+
+Use schedulingDB;
+INSERT INTO users (firstName, lastName, email, phone, isManager, contactMethod)
+VALUES ('Test', 'User', 'test.user@test.com', '123-456-7890', false, 'phone');
