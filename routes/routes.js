ee4ffe7ec3e63eb6ca699625ec8ba7a869d49935
Matthew Keatley <matthew.keatley@nordstrom.com>
2019-09-01 09:19:54 -0700

    add shifts routes

--- routes/routes.js
+++ routes/routes.js
@@ -10,9 +10,10 @@ router.post('/api/v1/users', Users.post);
 router.put('/api/v1/users/:id', Users.put);
 router.delete('/api/v1/users/:id', Users.delete);
 
-router.get('/api/v1/shifts', Shifts.getAll);
-// router.get('/api/v1/users/:id', Users.getById);
-// router.post('/api/v1/users', Users.post);
-// router.put('/api/v1/users/:id', Users.put);
+router.get('/api/v1/shifts/user/:userID', Shifts.getAll);
+router.get('/api/v1/shifts/:id', Shifts.getById);
+router.post('/api/v1/shifts/user/:userID', Shifts.post);
+router.put('/api/v1/shifts/:id', Shifts.put);
+router.delete('/api/v1/shifts/:id', Shifts.delete);
 
 export default router;
